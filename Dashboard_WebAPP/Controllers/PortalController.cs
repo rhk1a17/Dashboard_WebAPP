9bac24e7796f374e7a1ecd7716be14443432f7c9
Ri Khee <rhk1a17@soton.ac.uk>
2019-08-22 10:22:13 +0800

    small changes

--- Dashboard_WebAPP/Controllers/PortalController.cs
+++ Dashboard_WebAPP/Controllers/PortalController.cs
@@ -18,7 +18,7 @@ namespace Dashboard_WebAPP.Controllers
         public ActionResult PortalIndex()
         {
             var m = new PortalViewModel();
-            ViewData["selectedInverter_sunny"] = "";
+            ViewData["selectedInverter_sunny"] = "  ";
             viewbagData("TR Energy 0.99MWp");
             SqlStringRetrieve();
             ViewBag.myValue = 0;
