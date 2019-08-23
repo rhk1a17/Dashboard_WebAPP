832412af1f9304ce8ef3964de5242b0000cdd2c3
Ri Khee <rhk1a17@soton.ac.uk>
2019-08-23 18:32:42 +0800

    Changed to google map API

--- Dashboard_WebAPP/Controllers/PortalController.cs
+++ Dashboard_WebAPP/Controllers/PortalController.cs
@@ -22,6 +22,7 @@ namespace Dashboard_WebAPP.Controllers
             viewbagData("TR Energy 0.99MWp");
             SqlStringRetrieve();
             ViewBag.myValue = 0;
+            viewbagCondition();
             return View(m);
         }
 
@@ -35,6 +36,70 @@ namespace Dashboard_WebAPP.Controllers
             SqlStringRetrieve();
             ViewBag.myValue = 1;
             return View(m);
+        }
+
+        public void viewbagCondition()
+        {
+            ViewBag.TR_Con = Condition("TR Energy 0.99MWp");
+            ViewBag.TCMA_Con = Condition("TCMA 1MWp");
+            ViewBag.Toyo_Con = Condition("Toyo Tires Manufacturing (M)");
+            ViewBag.CLK_Con = Condition("Choo Lay Khuan 5kWp");
+            ViewBag.Nasha_Con = Condition("Nasharuddin 2017");
+            ViewBag.CCP_Con = Condition("ChuaChinPeng");
+            ViewBag.TCH_Con = Condition("tingchenhunt");
+            ViewBag.Shahrir_Con = Condition("Mohamad Shahrir");
+            ViewBag.LKY_Con = Condition("Loh Kiat Yoong");
+            ViewBag.Krishna_Con = Condition("Krishna");
+            ViewBag.Affandi_Con = Condition("Affandi");
+            ViewBag.LSK_Con = Condition("Lee Szed Kee");
+            ViewBag.OLK_Con = Condition("Ooi Lee Kean");
+            ViewBag.SS15_Con = Condition("SS15");
+            ViewBag.SKS_Con = Condition("Soo Kai Soon");
+            ViewBag.CEH_Con = Condition("Chua Eng Hin");
+            ViewBag.OHK_Con = Condition("Ooi Hoon Kong");
+            ViewBag.William_Con = Condition("William Lin Kee Wai");
+            ViewBag.CPW_Con = Condition("CP Wong");
+            ViewBag.NWX_Con = Condition("Ng Wei Xin");
+            ViewBag.SecretGarden_Con = Condition("secret-garden 6.3kWp Solar PV");
+            ViewBag.Kuvendran_Con = Condition("Kuvendran");
+            ViewBag.JOSL_Con = Condition("Jack Ong Seng Leong");
+            ViewBag.TCW_Con = Condition("Tan Chun Weng");
+            ViewBag.YKW_Con = Condition("Yim Kien Wai 11.925kWP");
+            ViewBag.LKP_Con = Condition("Lau Ken Poh");
+            ViewBag.LKM_Con = Condition("Lee Khek Mui");
+            ViewBag.CMY_Con = Condition("CM Yee");
+            ViewBag.Jerome_Con = Condition("Jerome Heah");
+            ViewBag.Choco_Con = Condition("ChocoPOW");
+            ViewBag.ChengPK_Con = Condition("ChengPK 22 S.Tropika U13/20B");
+            ViewBag.KPChurch_Con = Condition("Klang Presbyterian Church");
+            ViewBag.Aviva_Con = Condition("1773 Aviva Green");
+            ViewBag.TYW_Con = Condition("Tan Yu Wea");
+            ViewBag.FF_Con = Condition("Fresh Fishery 132kWp");
+            ViewBag.RoL_Con = Condition("River Of Life");
+            ViewBag.TPE_Con = Condition("Kraiburg-TPE");
+            ViewBag.Hercules_Con = Condition("Hercules F2");
+            ViewBag.SSL_Con = Condition("SSL 180kwp");
+            ViewBag.SFH_Con = Condition("Shea Fatt Hardware (M) Sdn Bhd");
+            ViewBag.SYW_Con = Condition("SYW INDUSTRY");
+            ViewBag.ICP_Con = Condition("ICP Ulu Choh");
+            ViewBag.Thumbprints_Con = Condition("Thumbprints Utd Sdn Bhd");
+            ViewBag.Firma_Con = Condition("FIRMA ODESI 1MW");
+        }
+
+        public string Condition(string Id)
+        {
+            string datestamp = Convert.ToDateTime(Timestamp(Id)).ToShortDateString();
+            string datenow = DateTime.Now.ToShortDateString();
+            string icon = String.Empty;
+            if (datestamp == datenow)
+            {
+                icon = "http://maps.google.com/mapfiles/ms/icons/green-dot.png";
+            }
+            else
+            {
+                icon = "http://maps.google.com/mapfiles/kml/pal3/icon37.png";
+            }
+            return icon;
         }
 
         public void viewbagData(string Id)
@@ -169,7 +234,6 @@ namespace Dashboard_WebAPP.Controllers
             // REQUIRED TIMESTRING 2019-08-16T113430 //"yyyy-MM-ddTHHmmss"
             string string1 = String.Concat("https://ers123storage.blob.core.windows.net/graphcontainer/",result);
             string graph_url = String.Concat(string1, ".png");
-            Debug.WriteLine(graph_url);
             return graph_url;
         }
 
@@ -305,7 +369,7 @@ namespace Dashboard_WebAPP.Controllers
             float totalEnergyToday = 0;
             float totalEnergyAllTime = 0;
 
-            string retrieve = "SELECT DISTINCT TOP 44 * FROM SUNNY_PORTAL_STRING WHERE CONVERT(date, _datetime) = FORMAT(GETDATE(), 'yyyy/MM/dd') ORDER BY real_datetime DESC;";
+            string retrieve = "SELECT DISTINCT TOP 44 * FROM SUNNY_PORTAL_STRING ORDER BY real_datetime DESC;";
 
             // SQL login data
             sql.DataSource = "sqlsever-ers.database.windows.net";   // Server name from azure
@@ -355,7 +419,7 @@ namespace Dashboard_WebAPP.Controllers
                                 totalEnergyToday += energy_today;
                             }
 
-                            if (float.TryParse(reader.GetTextReader(6).ReadToEnd().ToString(), out tempTotal) && total_energy_unit == "MWh")
+                            if (float.TryParse(reader.GetTextReader(6).ReadToEnd().ToString(), out tempTotal))
                             {
                                 float total_energy = tempTotal;
                                 totalEnergyAllTime += total_energy;
